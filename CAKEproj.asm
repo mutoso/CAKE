f3a713f3a4d17c70384712fa62c20e5fcd8e9577
cortonSE <clo140130@utdallas.edu>
2016-10-04 20:24:32 -0500

    Code file
    Messages only

---
+++ CAKEproj.asm
@@ -0,0 +1,8 @@
+.data
+promptmesg: .asciiz "Enter a word: "
+foundmesg: .asciiz "Found words: \n"
+scoremesg: .asciiz "Score: "
+nwlnmesg: .asciiz "\n"
+commandmesg: .asciiz "Commands: 1-shuffle, 2-quit"
+.text
+main:
