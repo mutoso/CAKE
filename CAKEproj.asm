00edc1c3774ec1c01638f63fdee960ae399ff0fd
cortonSE <clo140130@utdallas.edu>
2016-10-13 20:18:28 -0500

    Additional flow edits

--- CAKEproj.asm
+++ CAKEproj.asm
@@ -1,49 +1,42 @@
 .data
 promptmesg: .asciiz "Enter a word: "
 scoremesg: .asciiz "Score: "
-nwlnmesg: .asciiz "\n"
-tabmesg: .asciiz "\t"
+screenclear: .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n"
 commfoundmesg: .asciiz "Commands: 1-shuffle, 2-quit\nFound words: \n"
 exitmesg: .asciiz "Goodbye, thanks for playing!\n"
 input: .space 10
-templine: .space 8
+board: .space 19
 .text
 main:
-	#	clear the previous screen
+	li $v0, 4
+	la $a0, screenclear	# clear the previous screen
+	syscall
 	li $v0, 4
 	la $a0, commfoundmesg
 	syscall
-		#addi $sp, $sp, -8
-		#sw $ra, 0($sp)	# save the return value to the stack
 	jal printfound	# print the currently found set of words
-		#lw $ra, 0($sp)	# now retrieve the return address from the previous call
-		#addi $sp, $sp, 8
 	li $v0, 4
 	la $a0, scoremesg
 	syscall
-	# display the score
 	li $v0, 1
-	add $a0, $s7, $zero
+	add $a0, $s7, $zero		# assuming score is in $s7
+	syscall		# display the score
+	li $v0, 4
+	la $a0, board	# print the board
 	syscall
-		#addi $sp, $sp, -8
-		#sw $ra, 0($sp)	# save the return value to the stack
-	jal printboard	# print the current board
-		#lw $ra, 0($sp)	# now retrieve the return address from the previous call
-		#addi $sp, $sp, 8
 	li $v0, 4
 	la $a0, promptmesg
 	syscall
-	
-	
+	#	get the input	
 	li $v0, 8
 	la $a0, input
 	li $a1, 10
 	syscall
 	# get first char into $t0
-	# if command 1
-		# beq $t0, 1, shuffle
-	# if command 2
-		# bne $t0, 2, processing
+		# if command 1
+	#beq $t0, 1, shuffle
+		# if command 2
+	#bne $t0, 2, processing
 	#li $v0, 4
 	#la $a0, exitmesg
 	#syscall
@@ -58,10 +51,3 @@ processing:
 	
 	
 printfound:
-
-printboard:
-	# board stored in array
-	# contruct first line in templine
-	li $v0, 4
-	la $a0, templine
-	syscall
