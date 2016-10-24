95fee66c44da4011db477dff57a15bc93bdbdba8
cortonSE <clo140130@utdallas.edu>
2016-10-24 15:09:07 -0500

    +Commands
    Added command processing

--- CAKEproj.asm
+++ CAKEproj.asm
@@ -4,6 +4,8 @@ scoremesg: .asciiz "Score: "
 screenclear: .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n"
 commfoundmesg: .asciiz "Commands: 1-shuffle, 2-quit\nFound words: \n"
 exitmesg: .asciiz "Goodbye, thanks for playing!\n"
+commOne: .byte '1'
+commTwo: .byte '2'
 input: .space 10
 board: .space 19
 .text
@@ -27,21 +29,20 @@ main:
 	li $v0, 4
 	la $a0, promptmesg
 	syscall
-	#	get the input	
-	li $v0, 8
+	li $v0, 8	# get the input
 	la $a0, input
 	li $a1, 10
 	syscall
-	# get first char into $t0
-		# if command 1
-	#beq $t0, 1, shuffle
-		# if command 2
-	#bne $t0, 2, processing
-	#li $v0, 4
-	#la $a0, exitmesg
-	#syscall
-	#li $v0, 4
-	#syscall
+	lw $t1, 0($a0)	# get first char into $t1
+	lw $t2, commOne
+	beq $t1, $t2, shuffle	# if command 1
+	lw $t2, commTwo
+	bne $t1, $t2, processing	# else if command 2
+	li $v0, 4		# if it's not command 2, 'exit', we skip this and go on to the processing
+	la $a0, exitmesg	# say goodbye
+	syscall
+	li $v0, 10
+	syscall
 shuffle:
 	# shuffle the board
 	
