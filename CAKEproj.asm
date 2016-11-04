a8851e2231ae9baf45c2b11c62636faee930fb2f
cortonSE <clo140130@utdallas.edu>
2016-11-03 19:45:16 -0500

    StrLen
    Now with string length!

--- CAKEproj.asm
+++ CAKEproj.asm
@@ -4,8 +4,11 @@ scoremesg: .asciiz "Score: "
 screenclear: .asciiz "\n\n\n\n\n\n\n\n\n\n\n\n\n"
 commfoundmesg: .asciiz "Commands: 1-shuffle, 2-quit\nFound words: \n"
 exitmesg: .asciiz "Goodbye, thanks for playing!\n"
-commOne: .word 1
-commTwo: .word 2
+errormesg: .asciiz "Error: Invalid word.\n"
+usedwordmesg: .asciiz "Error: Word already used.\n"
+commOne: .word 0x31
+commTwo: .word 0x32
+newLine: .word 0x0A
 input: .space 10
 board: .space 19
 fout: .asciiz "board.txt"
@@ -13,6 +16,7 @@ reservedspace: .space 2048
 
 .text
 main:
+	jal loadDictionary # load dictionary
 	li $v0, 4
 	la $a0, screenclear	# clear the previous screen
 	syscall
@@ -36,21 +40,12 @@ main:
 	la $a0, input
 	li $a1, 10
 	syscall
-	lw $t1, 0($a0)	# get first char into $t1
-	#move $t1, $a0       # not sure the command above works properly??
+	lb $t1, 0($a0)	# get first char into $t1
 	lw $t2, commOne
 	beq $t1, $t2, shuffle	# if command 1
 	lw $t2, commTwo
-	bne $t1, $t2, processing	# else if command 2
-	#li $v0, 4		# if it's not command 2, 'exit', we skip this and go on to the processing
-	#la $a0, exitmesg	# say goodbye
-	#syscall
-	#li $v0, 10
-	#syscall
-	
-	jal loadDictionary # load dictionary
-	
-	j exit
+	bne $t1, $t2, processing	# else if not command 2
+	j exit		# if command 2, exit program
 	
 loadDictionary:
     li $v0, 13
@@ -84,10 +79,42 @@ shuffle:
 	
 processing:
 	# process the user input
+		# if string length is <4
+	jal stringLength
+	move $t2, $v0
+	add $t3, $zero, 4
+	slt $t1, $t2, $t3
+	beq $t1, 1, invalidString
+	# continue processing
 	j main
 	
+stringLength:
+	add $t0, $t0, $zero	# initialize count to 0
+strLenLoop:
+	lb $t1, 0($a0) # load a character into t1
+	lw $t2, newLine
+	beq $t1, $t2, exitStrLen	# exit the loop if we have a newLine character
+	beq $t1, $zero exitStrLen	# exit the loop if we have a null character
+	addi $a0, $a0, 1 #load increment string pointer
+	addi $t0, $t0, 1 #increment count
+	j strLenLoop
+exitStrLen:
+	move $v0, $t0
+	jr $ra
 	
+invalidString:
+	li $v0, 4
+	la $a0, errormesg
+	syscall
+	# jr $ra	# ???
+	# j main	# ???
+
 printfound:
 	jr $ra
 
 exit:
+	li $v0, 4		# if it's not command 2, 'exit', we skip this and go on to the processing
+	la $a0, exitmesg	# say goodbye
+	syscall
+	li $v0, 10
+	syscall
